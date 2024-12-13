208c87220daf47e16f0e0d2194f06bf025e58f74
SAHAN20002 <sahan.waruna1234@gmail.com>
2024-12-14 00:35:18 +0530

    AWS Config

--- src/main/java/com/ecom/product/api/service/FileService.java
+++ src/main/java/com/ecom/product/api/service/FileService.java
@@ -4,8 +4,10 @@ import com.ecom.product.api.dto.CommonFileSavedBinaryDataDto;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-@Service
+import java.io.IOException;
+
+
 public interface FileService {
-    public CommonFileSavedBinaryDataDto create(MultipartFile file,String directory ,String bucket);
+    public CommonFileSavedBinaryDataDto create(MultipartFile file,String directory ,String bucket) throws IOException;
     public void delete(String directory,String fileName ,String bucket);
 }
--- src/main/java/com/ecom/product/api/service/impl/FileServiceImpl.java
+++ src/main/java/com/ecom/product/api/service/impl/FileServiceImpl.java
@@ -1,13 +1,32 @@
 package com.ecom.product.api.service.impl;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.ecom.product.api.dto.CommonFileSavedBinaryDataDto;
 import com.ecom.product.api.service.FileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
 public class FileServiceImpl implements FileService {
+
+    private final AmazonS3 amazonS3;
+    private final AmazonS3Client amazonS3Client;
     @Override
-    public CommonFileSavedBinaryDataDto create(MultipartFile file, String directory, String bucket) {
-        return null;
+    public CommonFileSavedBinaryDataDto create(MultipartFile file, String directory, String bucket) throws IOException {
+        String originalFileName = file.getOriginalFilename();
+        String newFileName = UUID.randomUUID()+"_"+originalFileName;
+        amazonS3Client.putObject(
+                new PutObjectRequest(bucket,directory+newFileName,file.getInputStream(),new ObjectMetadata()).withCannedAcl(CannedAccessControlList.PublicRead)
+        );
     }
 
     @Override
